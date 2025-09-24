e56d4e769586260432f7e0c38906ef9ffcbfd4fb
Dariac <102596052+Cairad@users.noreply.github.com>
2025-09-24 16:19:26 -0300

    Update urls.py

--- crud/crud/urls.py
+++ crud/crud/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('mantenedores.urls'))
+    path('', include('mantenedores.urls')),
+    path('api/', include('mantenedores.api_urls')),
 ]
